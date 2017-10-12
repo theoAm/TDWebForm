b7fb7a44686dc15094039f94bc812f9f78985094
thodoris <theo_am@yahoo.com>
2017-10-12 13:40:26 +0300

    open ssl encrypt

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,25 @@
 |
 */
 
+$router->get('test', function () {
+
+    $plain = 'Seldaek' . $_ENV['STRING_DELIMITER'] . 'composer';
+    $cipher = $_ENV['CIPHER'];
+    $options = 0;
+    $ivlen = openssl_cipher_iv_length($cipher);
+    $key = substr($_ENV['APP_KEY'], '0', $ivlen);
+
+    $iv = openssl_random_pseudo_bytes($ivlen);
+    $encrypt = openssl_encrypt($plain, $cipher, $key, $options, $key);
+
+    var_dump($encrypt);echo '<br>';
+
+    $decrypt = openssl_decrypt($encrypt, $cipher, $key, $options, $key);
+
+    var_dump($decrypt);
+
+});
+
 $router->group(['middleware' => 'cors'], function () use ($router) {
 
     $router->get('/', 'MainController@index');
