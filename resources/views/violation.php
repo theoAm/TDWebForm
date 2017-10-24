c259fe01b6e1911d77094210fc575e3cbe1924fd
theo_am <theo_am@yahoo.com>
2017-10-24 20:00:12 +0300

    viewport

--- resources/views/violation.php
+++ resources/views/violation.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Technical Debt Evaluation</title>
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
